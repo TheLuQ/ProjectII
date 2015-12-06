48fc709154729984003ad3c6bf134bf229244c14
TheLuQ <mistrz.luq@gmail.com>
2015-12-06 13:46:26 +0100

    Menu - gotowe

--- Convolution.cpp
+++ Convolution.cpp
@@ -10,10 +10,34 @@ using namespace std;
 
 int main()
 {
-	bool menu = true;
+	int framesBits, frames = 0;
+	float EbN0min, EbN0max, EbN0step = 0;
+
+
+	bool menu = false;
 	if (menu == true){
-		cout << "Podaj dane:\nLiczba bitow w ramce: " << endl;
+		cout << "Podaj dane:\nLiczba ramek: \n";
+		cin >> frames;
+		cout << "Liczba bitow w ramce: \n";
+		cin >> framesBits;
+		cout << "EbN0 max: \n";
+		cin >> EbN0max;
+		cout << "EbN0 min: \n";
+		cin >> EbN0min;
+		cout << "EbN0 step: \n";
+		cin >> EbN0step;
 	}
+	else {
+		frames = 1;
+		framesBits = 15;
+	}
+
+
+
+
+
+
+
     return 0;
 }
 
