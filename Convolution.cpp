74ed4b40d991bfba5ab2af2fa0bfcabfd588e05d
TheLuQ <mistrz.luq@gmail.com>
2015-12-06 12:55:01 +0100

    Menu - początek

---
+++ Convolution.cpp
@@ -0,0 +1,19 @@
+// Convolution.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+using namespace std;
+
+
+int main()
+{
+	bool menu = true;
+	if (menu == true){
+		cout << "Podaj dane:\nLiczba bitow w ramce: " << endl;
+	}
+    return 0;
+}
+
