cf0cb0fe343e43f7d60b537299428a82f041cbf3
TheLuQ <mistrz.luq@gmail.com>
2015-12-07 03:40:57 +0100

    Dekoder zaczęty
    nie działa odl. hamminga między seqOut a inputem

--- Convolution.cpp
+++ Convolution.cpp
@@ -17,6 +17,10 @@ void readMat(Vec mat);
 Vec matMul( Vec& a,  Matrix& b); //a może być tablicą JEDNOWYMIAROWĄ
 Matrix matMul(Matrix& a, Matrix& b); //w razie potrzeby mnożenia pełnych macierzy - chyba nie przyda się 
 int odlHamm(Vec& a, Vec& b);
+Vec intToVec(int intNumber);
+int VecToInt(Vec binNumb);
+Vec seqOut(Vec prevState, Matrix coder);
+Vec addAndMove(int bitIn, Vec& prevState);
 
 
 int main()
@@ -75,10 +79,53 @@ int main()
 	
 	
 	
-	readMat(bitsChannel); // macierz z przesłanymi sekwencjami wyjsciowymi
+	//readMat(bitsChannel); // macierz z przesłanymi sekwencjami wyjsciowymi
 
+
+
+//===================================================== >>> DEKODER<<< ==========================================
+	int nrRej = 4;
+	int maxRej = 16;
+	Vec prevState(4, 0);
+	Matrix pathes(2* maxRej, Vec(framesBits)); // 4 rejestry 2^4
+	Vec hamm(2 * maxRej,0);
+	int ii = 0; // do pathes uzupelnianie
+
+
+	Matrix tempBitsChannel(2 * maxRej, Vec(6));
+
+	for (int i = 0; i < maxRej; i++) {
+		Vec temp2(nrRej, 0); 
+		Vec temp = intToVec(i);
+		if (temp.size() < maxRej) {
+			Vec fillUp(nrRej - temp.size(), 0);
+			copy(temp.begin(), temp.end(), temp2.begin()+ nrRej - temp.size());	
+		}
+		copy(temp2.begin(), temp2.end(), pathes[ii].begin());
+		//tempBitsChannel[ii] = bitsChannel[i];
+		ii++;
+		copy(temp2.begin(), temp2.end(), pathes[ii].begin());
+		//tempBitsChannel[ii] = bitsChannel[i];			// NIE DZIALA
+		ii++;
+	}
+	readMat(pathes);
+	// @@@ wstepne uzupelnianie tablicy pathes zakonczone
 	
+	for (int i = 0; i < 2*maxRej; i++) {
+		readMat(pathes[i]);
+		for (int j = 0; j < nrRej; j++) {
+			addAndMove(pathes[i][j], prevState); // RACZEJ DZIALA
 
+			//hamm[i] += odlHamm(seqOut(prevState, coder), tempBitsChannel[i]);
+			//seqOut(prevState, coder);
+			prevState.pop_back();
+		}
+		cout << "\n\n\n";
+		prevState = Vec(4, 0);
+
+	}
+
+	readMat(hamm);
 
     return 0;
 }
@@ -160,4 +207,50 @@ int odlHamm(Vec& a, Vec& b) {
 	}
 	return odl;
 
+}
+
+Vec intToVec(int intNumber) {
+	Vec binaryVec;
+
+	while (intNumber>0)
+	{
+		binaryVec.push_back(intNumber % 2);
+		intNumber = intNumber / 2;
+	}
+	reverse(binaryVec.begin(), binaryVec.end());
+	return binaryVec;
+}
+
+int VecToInt(Vec binNumb) {
+
+	int suma = 0;
+	for (int i = 0; i < binNumb.size(); i++) {
+		suma += binNumb[i] * pow(2, binNumb.size() - 1  - i);
+	}
+	return suma;
+}
+
+/*
+Vec seqOut(int bitIn, Vec prevState, Matrix coder) {
+
+	Vec temp;
+	Vec seq;
+	Vec::iterator it = temp.begin(); // pozycja iteratora
+	it = temp.insert(it, bitIn);
+	seq = matMul(temp, coder);
+	return seq;
+}
+
+*/
+Vec seqOut(Vec prevState, Matrix coder) {
+
+	Vec seq;
+	seq = matMul(prevState, coder);
+	return seq;
+}
+
+Vec addAndMove(int bitIn, Vec& prevState) {
+	Vec::iterator it = prevState.begin(); // pozycja iteratora
+	it = prevState.insert(it, bitIn);
+	return prevState;
 }
