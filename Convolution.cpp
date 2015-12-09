a4ba2ec7d41dfea6c0dd7ce833bf0e7bf0cc2e05
TheLuQ <mistrz.luq@gmail.com>
2015-12-09 23:40:58 +0100

    DZIAŁA KURWA
    FUUUCK YEAH
    Obserwacja: zmiana ostatniego bitu nie jest wykrywana

--- Convolution.cpp
+++ Convolution.cpp
@@ -21,6 +21,7 @@ Vec intToVec(int intNumber);
 int VecToInt(Vec binNumb);
 Vec seqOut(Vec prevState, Matrix coder);
 Vec addAndMove(int bitIn, Vec& prevState);
+int stateToInt(Vec State); // zwraca numer następnego stanu
 
 
 int main()
@@ -56,7 +57,7 @@ int main()
 		
 	}
 	else {
-		bitsIn = { 1, 0, 1,0, 1,1,0,0,1,1,0,0,0,0 };
+		bitsIn = { 1, 0, 1,0, 1,1,0,0,1,1,0,0,0,1,0 };
 		frames = 1;
 		framesBits = bitsIn.capacity();
 	}
@@ -72,11 +73,9 @@ int main()
 	for (int i = 0; i < bitsIn.size(); i++) {
 		Vec::iterator it = prev.begin(); // pozycja iteratora
 		it = prev.insert(it, bitsIn[i]);
-		prev.pop_back();
 		bitsChannel.push_back(seqOut(prev, coder));
+		prev.pop_back();
 	}
-	
-	
 	
 	//readMat(bitsChannel); // macierz z przesłanymi sekwencjami wyjsciowymi
 
@@ -88,8 +87,11 @@ int main()
 	Vec prevState(4, 0);
 	Matrix tempPathes(2* maxRej, Vec(framesBits)); // 4 rejestry 2^4 // tymczasowe wszyskie scieżki
 	Matrix pathes(maxRej, Vec(framesBits));		// wszystkie ocalone ścieżki
+	Vec lastState(2*maxRej, 0);	// ostatni stan każdej możliwej ścieżki
+	Vec findDuplHamm(maxRej, 0); 
 	Vec survInx(maxRej,0);		// ocalałe indexy ścieżek o najmniejszym hammingu
 	Vec hamm(2 * maxRej,0);
+	Vec savedHamm(maxRej, 0);
 	int ii = 0; // do pathes uzupelnianie
 
 	for (int i = 0; i < maxRej; i++) { // pierwsze uzupelnienie pathes
@@ -119,7 +121,12 @@ int main()
 		}
 		prevState = Vec(4, 0);
 	}
+
+	
+	for (int i = 0; i < 2*maxRej; i=i+2)
+		savedHamm[i/2] = hamm[i];
 	readMat(hamm);
+
 
 
 	for (int b = nrRej; b < framesBits - 1; b++) { //b-ty bit wejscia
@@ -129,23 +136,64 @@ int main()
 
 			copy(pathes[i].begin(), pathes[i].begin() + b, tempPathes[ii].begin());
 			tempPathes[ii][b] = ii % 2;
+			hamm[ii] = savedHamm[i];
 			ii++;
 			copy(pathes[i].begin(), pathes[i].begin() + b, tempPathes[ii].begin());
 			tempPathes[ii][b] = ii % 2;
+			hamm[ii] = savedHamm[i];
 			ii++;
 		}
+
+
+
+		survInx = Vec(maxRej, 0);
 
 		for (int j = 0; j < 2*maxRej; j++) { // j-ta ścieżka
 			vector<int> prevState;
 			prevState.assign(tempPathes[j].begin() + b - nrRej, tempPathes[j].begin() + b + 1);
 			reverse(prevState.begin(), prevState.end());
 			hamm[j] += odlHamm(seqOut(prevState, coder), bitsChannel[b]);
+			lastState[j] = stateToInt(prevState);
+
+			cout << lastState[j] << endl;
+
+			if (findDuplHamm[lastState[j]] == 0) {
+				findDuplHamm[lastState[j]] = hamm[j];
+				survInx[lastState[j]] = j;
+			}
+			else if (findDuplHamm[lastState[j]] > hamm[j]) {
+				findDuplHamm[lastState[j]] = hamm[j];
+				survInx[lastState[j]] = j;
+			}
+
+			if (hamm[j] == 0)
+				findDuplHamm[lastState[j]] = -1;	//żeby algorytm nie mógł nadpisać nad nią jakiejś ścieżki
+			
+			//cout << hamm[j] << endl;
+			//readMat(findDuplHamm); // dobre sprawdzanie !!!
 
 		}
 
 
-	}
+		for (int i = 0; i < findDuplHamm.size(); i++) {
+			if (findDuplHamm[i] == -1)
+				findDuplHamm[i] == 0;
+		}
 
+		//prevState.assign(tempPathes[j].begin() + b - nrRej, tempPathes[j].begin() + b + 1);
+		//copy(pathes[i].begin(), pathes[i].begin() + b, tempPathes[ii].begin());
+		for (int i = 0; i < maxRej; i++) {
+			copy(tempPathes[survInx[i]].begin(), tempPathes[survInx[i]].begin() + b + 2, pathes[i].begin()); // CZY NA PEWNO + 1 ? // + 2 
+			savedHamm[i] = hamm[survInx[i]];
+		}
+		
+		readMat(savedHamm);
+		findDuplHamm = Vec(maxRej, 0);
+		cout << endl;
+		cout << endl;
+	}
+	
+	readMat(pathes);
     return 0;
 }
 //===================================================== >>> FUNKCJE <<< ==========================================
@@ -250,18 +298,6 @@ int VecToInt(Vec binNumb) {
 
 }
 
-/*
-Vec seqOut(int bitIn, Vec prevState, Matrix coder) {
-
-	Vec temp;
-	Vec seq;
-	Vec::iterator it = temp.begin(); // pozycja iteratora
-	it = temp.insert(it, bitIn);
-	seq = matMul(temp, coder);
-	return seq;
-}
-
-*/
 Vec seqOut(Vec prevState, Matrix coder) {
 
 	Vec seq;
@@ -273,4 +309,10 @@ Vec addAndMove(int bitIn, Vec& prevState) {
 	Vec::iterator it = prevState.begin(); // pozycja iteratora
 	it = prevState.insert(it, bitIn);
 	return prevState;
+}
+
+int stateToInt(Vec State) {
+	State.pop_back();
+	return VecToInt(State);
+
 }
